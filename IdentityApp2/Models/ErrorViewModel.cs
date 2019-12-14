76ccd34c48186be855c81d41a8439dc01da3fc13
tima-kh-97 <tima.kh.97@gmail.com>
2019-12-14 09:47:25 +0600

    Project Milestone 4

---
+++ IdentityApp2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
